e42a540ef911eba60c357348a164702d3096d759
infosmith <dst.dsmith@gmail.com>
2018-12-26 05:01:21 -0500

    refactor
    Why:
    
    * Avoid premature architecture decisions. Mixin abstract
    base class was to be used to insert data and behavior
    later if needed.
    
    This change addresses the need by:
    
    * Remove stub mixin superclass.

--- batteries/__init__.py
+++ batteries/__init__.py
@@ -1,11 +1,5 @@
 """Top-level package of batteries."""
-from abc import ABC
-
 
 __author__ = """David S."""
 __email__ = 'infosmith@prontonmail.com'
 __version__ = '0.0.0'
-
-
-class Mixin(ABC):
-    pass
